5e5cafe198ca4850981e06c6ef527dce05d4bc69
constXife <constxife@yandex.ru>
2017-07-14 19:15:46 +0700

    Fix current_user

--- app/controllers/issue_cancel_controller.rb
+++ app/controllers/issue_cancel_controller.rb
@@ -2,11 +2,11 @@ class IssueCancelController < ApplicationController
   # unloadable
 
   def create
-    
+
     current_user = User.current
     @issue = Issue.visible.find(params[:id])
 
-    
+
     issue_status = Setting.plugin_redmine_issue_cancel['cancel_issue_statuses']
 
     @issue.status_id = issue_status
@@ -16,17 +16,18 @@ class IssueCancelController < ApplicationController
 
     @last_journal    = @issue.journals.visible.order('created_on').last
 
-    notes = "Параметр Назначена изменился на #{current_user.name}, Параметр Статус изменился на #{@issue.status.name}"
+    notes = "Параметр Статус изменился на #{@issue.status.name}"
+    notes += ". Параметр Назначена изменился на #{current_user.name}" if current_user
     @issue.init_journal(current_user, notes.html_safe)
     @issue.save
 
     if @last_journal.nil?
       redirect_to "#{issue_path(@issue)}"
     else
-     
-      @last_journal = @issue.journals.visible.order('created_on').last    
+
+      @last_journal = @issue.journals.visible.order('created_on').last
     redirect_to "#{issue_path(@issue)}#change-#{@last_journal.id}"
     end
   end
-  
+
 end
