e9dae1f07d0f2fcf4a2da805d903ed1d97ed8e65
Buraga Dmitrii <buraga.dmitrii@gmail.com>
2017-07-18 19:21:02 +0300

    Output system info about changes

--- app/controllers/issue_cancel_controller.rb
+++ app/controllers/issue_cancel_controller.rb
@@ -5,21 +5,37 @@ class IssueCancelController < ApplicationController
 
     current_user = User.current
     @issue = Issue.visible.find(params[:id])
+    issue_old_status = @issue.status.name
+    old_user         = @issue.assigned_to
 
 
     issue_status = Setting.plugin_redmine_issue_cancel['cancel_issue_statuses']
 
     @issue.status_id = issue_status
-    @issue.assigned_to = current_user
+    @issue.assigned_to = current_user if current_user
     @issue.save
 
+    
+    @new_journal = @issue.init_journal(current_user)
+    @new_journal.save!
 
     @last_journal    = @issue.journals.visible.order('created_on').last
 
-    notes = "Параметр Статус изменился на #{@issue.status.name}"
-    notes += ". Параметр Назначена изменился на #{current_user.name}" if current_user
-    @issue.init_journal(current_user, notes.html_safe)
-    @issue.save
+    JournalDetail.new({
+      :property  => 'attr',
+      :prop_key  => 'status',
+      :old_value => issue_old_status,
+      :value     => @issue.status.name,
+      :journal   => @new_journal
+    }).save!
+
+    JournalDetail.new({
+      :property  => 'attr',
+      :prop_key  => 'user',
+      :old_value => old_user,
+      :value     => current_user.name,
+      :journal   => @new_journal
+    }).save!
 
     if @last_journal.nil?
       redirect_to "#{issue_path(@issue)}"
