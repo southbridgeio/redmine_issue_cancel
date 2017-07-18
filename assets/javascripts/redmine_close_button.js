13abec6d1a6c45196494f9d38ab5c7600045d5cc
Buraga Dmitrii <buraga.dmitrii@gmail.com>
2017-07-18 18:00:10 +0300

    Changed style and minified js code

--- assets/javascripts/redmine_close_button.js
+++ assets/javascripts/redmine_close_button.js
@@ -1,6 +1,5 @@
 (function() {
 	
-	var ID_CLOSED = 5, ID_REJECTED = 6;
 	if (window.jQuery) {
 		// redmine uses jQuery so use it.
 		jQuery(document).ready(function() {
@@ -9,53 +8,22 @@
 			if (s.length === 0) {
 				return;
 			}
-			var options = s.get(0).childNodes;
-			var needCloseButton = false;
-			for (var i = options.length - 1; i >= 0; i--) {
-				var option = options[i];
-				var v = parseInt(option.value, 10);
-				if (v === ID_REJECTED) {
-					if (option.selected) {
-						needCloseButton = false;
-						break;
-					}
-				}
-				if (v === ID_CLOSED) {
-					if (option.selected) {
-						needCloseButton = false;
-						break;
-					} else {
-						needCloseButton = true;
-					}
-				}
-			}
-			if (!needCloseButton) {
-				return;
-			}
+		 
 			var f = $('#issue-form');
 			var areas = $('div#content>div.contextual:has(a.icon)');
 			if (f.length === 0 || areas.length === 0) {
 				return;
 			}
-			var done_ratio = $('#issue_done_ratio');
-			var closer = function(e) {
-				s.val(ID_CLOSED);
-				done_ratio.val(100);
-				f.submit();
-			};
+		
 			var closeButtonTemplate = $('a.redmine-close-button');
 			areas.each(function() {
 				var closeButton = closeButtonTemplate
 					.clone()
 					.css('display', 'inline')
 					.click(closer);
-
-				var delButton = $(this).find('a.icon-del');
-				if (delButton.length > 0) {
-					closeButton.insertBefore(delButton);
-				} else {
+ 
 					$(this).append(closeButton);
-				}
+				 
 			});
 		});
 	} else {
@@ -65,29 +33,7 @@
 			if (s === null) {
 				return;
 			}
-			var options = s.childNodes;
-			var needCloseButton = false;
-			for (var i = options.length - 1; i >= 0; i--) {
-				var option = options[i];
-				var v = parseInt(option.value, 10);
-				if (v === ID_REJECTED) {
-					if (option.selected) {
-						needCloseButton = false;
-						break;
-					}
-				}
-				if (v === ID_CLOSED) {
-					if (option.selected) {
-						needCloseButton = false;
-						break;
-					} else {
-						needCloseButton = true;
-					}
-				}
-			}
-			if (!needCloseButton) {
-				return;
-			}
+			 
 			var f = $('issue-form');
 			var all_areas = $$('div#content>div.contextual');
 			var areas = [];
@@ -100,26 +46,15 @@
 			if (f === null || areas.length === 0) {
 				return;
 			}
-			var done_ratio = $('issue_done_ratio');
-			var closer = function(e) {
-				s.value = ID_CLOSED;
-				if (done_ratio) {
-					done_ratio.value = 100;
-				}
-				f.submit();
-			};
+			
 			var closeButtonTemplate = $$('a.redmine-close-button')[0];
 			for (ai = 0; ai < areas.length; ai++) {
 				var area = areas[ai];
 				var closeButton = closeButtonTemplate.cloneNode(true);
 				closeButton.style.display = 'inline';
 				closeButton.observe('click', closer);
-				var delButton = area.select('a.icon-del');
-				if (delButton.length > 0) {
-					delButton[0].insert({ before: closeButton });
-				} else {
-					area.appendChild(closeButton);
-				}
+			
+				area.appendChild(closeButton);
 			}
 		});
 	}
